6ec1810a647cfa14e1239cd1753bd0e392214162
Yaneli Cruz <yanelicruz092@gmail.com>
2026-04-24 16:36:50 -0700

    added personal token to .gitignore

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,6 +14,12 @@ import javax.swing.SwingUtilities;
 // OR YOU CAN HOVER YOUR MOUSE OVER THE SQUARES TO SEE THE FILE NAMES AND # OF LINES IN THE FILE.
 // I used the following github repository to test the application: https://github.com/javiergs/TULIP
 
+// It takes a second to load...
+
+// I have a tulip.properties file I didn't commit/aren't attaching with my personal token, 
+// so if you want to test the application, create a tulip.properties file in the src/main/resources directory 
+// with your own token in the following format: GITHUB_TOKEN=ghp_yourtokenhere
+
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
